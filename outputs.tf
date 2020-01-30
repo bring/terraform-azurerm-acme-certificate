f82431355b8e91453fc8edd799ca040e92fcd515
Jon Vassbø <jon.vassbo@bring.com>
2020-01-30 10:33:08 +0100

    Add certificate PEM output

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,7 @@ output "ceritificate_p12" {
 output "ceritificate_p12_password" {
   value = acme_certificate.domain-cert.certificate_p12_password
 }
+
+output "ceritificate_pem" {
+  value = acme_certificate.domain-cert.certificate_pem
+}
