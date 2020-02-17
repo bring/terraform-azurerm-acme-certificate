df6ec3444da08524a6dd256e5a4f8ee19f3646ff
Jon Vassbø <jon.vassbo@bring.com>
2020-02-17 11:09:23 +0100

    Set TF specific variables to avoid override

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "random_string" "cert-password" {
 
 provider "acme" {
   server_url = "https://acme-v02.api.letsencrypt.org/directory"
-  version = "1.5.0"
+  version    = "1.5.0"
 }
 
 resource "acme_certificate" "domain-cert" {
@@ -37,6 +37,11 @@ resource "acme_certificate" "domain-cert" {
       AZURE_SUBSCRIPTION_ID = var.dns_subscription_id
       AZURE_TENANT_ID       = var.dns_tenant_id
       AZURE_RESOURCE_GROUP  = var.dns_resource_group
+      ARM_CLIENT_ID         = module.mgmt_key_vault.secrets_values[var.sp_client_id]
+      ARM_CLIENT_SECRET     = module.mgmt_key_vault.secrets_values[var.sp_client_secret]
+      ARM_SUBSCRIPTION_ID   = var.dns_subscription_id
+      ARM_TENANT_ID         = var.dns_tenant_id
+      ARM_RESOURCE_GROUP    = var.dns_resource_group
     }
   }
 }
